e54cabe468e071432eb903d26352fb092725c5bd
Amarrah Len <amarrahlen@gmail.com>
2024-04-28 01:28:57 -0700

    react tutorial video

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,6 +2,14 @@ function Header(){
     return(
         <header>
             <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">contact</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }

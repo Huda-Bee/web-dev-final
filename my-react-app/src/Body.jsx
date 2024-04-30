7659eb8ffb4ac75b446334e84aa46e7e2470c5f5
Amarrah Len <amarrahlen@gmail.com>
2024-04-29 21:24:15 -0700

    customized w text box and banner

--- my-react-app/src/Body.jsx
+++ my-react-app/src/Body.jsx
@@ -1,7 +1,10 @@
 function Body() {
     return(
-        <body>
-           <p1>x</p1>
+        <body>  
+        <p class="one">Amarrah <br/> Food   </p>
+        <img class = "michael" src="https://i.ebayimg.com/images/g/myUAAOSwYolgXQ~M/s-l1200.webp" ></img>
+
+        <p class="two">Sample Text four</p>
 
         </body>
     );

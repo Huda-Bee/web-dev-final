8141af5ec073a68d580751c4d1f9390b41bc41ff
Amarrah Len <amarrahlen@gmail.com>
2024-05-01 19:54:07 -0700

    custom

--- my-react-app/src/Body.jsx
+++ my-react-app/src/Body.jsx
@@ -10,12 +10,23 @@ function Body() {
                 <li><a href="#">contact</a></li>
             </ul>
         </nav>
-        <p class="one">Hi  <br/> xxx   </p>
+
+        <div class="text">
+            <div class="right">
+            <p id="one">Hi  <br/> xxx   </p>
+            </div>
+        </div>
+
         <img class="graphic" src="https://swirl.neocities.org/sozai/line/linebutterfuly.gif"></img>
         <img class="graphic2" src="https://swirl.neocities.org/sozai/line/linebutterfuly.gif"></img>
-        <div class="middle">
-        <p class="two"> Hi! My name's Amarrah, and I'm a first year BioE major at UC Berkeley. I'm from San Jose, CA and...</p>
+
+        <div class = "text">
+            <div class="middle">
+                <p id="two"> Hi! My name's Amarrah, and I'm a BioE major at UC Berkeley. I enjoy men and nature and r&b music...</p>
+                <p id ="bottom-text"></p>
+            </div>
         </div>
+
         </body>
 
     );
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return(
         <header>
-            <h1>Amarrah's Blog</h1>
+            <h1>Amarrah and Huda's Blog</h1>
             <div class="middle">
             <h2>Welcome!</h2>
             </div>

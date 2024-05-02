29b58d6133a9100c6408da66b771e51be511d6c2
Amarrah Len <amarrahlen@gmail.com>
2024-05-02 14:15:02 -0700

    working on banners

--- my-react-app/src/Body.jsx
+++ my-react-app/src/Body.jsx
@@ -1,31 +1,39 @@
+
 function Body() {
     return(
         <body>
+        <div class = "box19" > <div class = "box-top" > <div class = "u01" ></div> </div>
 
+        <div class = "box-center" > <div class = "box-inner" >text </div> </div>
+
+        <div class = "box-bottom" > <div class = "s01" ></div> </div> </div>
+
+        <div class ="background.one">
+            <p id="strawberry"></p>
+            </div>
         <nav>
-            <ul>
+            <ul id="buttons">
                 <li><a href="#">home</a></li>
                 <li><a href="#">about</a></li>
                 <li><a href="#">services</a></li>
+                <li><a href="#">pictures</a></li>
                 <li><a href="#">contact</a></li>
             </ul>
         </nav>
 
-        <div class="text">
             <div class="right">
             <p id="one">Hi  <br/> xxx   </p>
             </div>
-        </div>
+
+            <div class="middle">
+            <p id="two"> Hi! My name's Amarrah, and I'm a BioE major at UC Berkeley. I enjoy men and nature and r&b music...</p>
+            <p id ="bottom-text"></p>
+            </div>
+
+        
 
         <img class="graphic" src="https://swirl.neocities.org/sozai/line/linebutterfuly.gif"></img>
         <img class="graphic2" src="https://swirl.neocities.org/sozai/line/linebutterfuly.gif"></img>
-
-        <div class = "text">
-            <div class="middle">
-                <p id="two"> Hi! My name's Amarrah, and I'm a BioE major at UC Berkeley. I enjoy men and nature and r&b music...</p>
-                <p id ="bottom-text"></p>
-            </div>
-        </div>
 
         </body>
 
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,9 +2,9 @@ function Header(){
     return(
         <header>
             <h1>Amarrah and Huda's Blog</h1>
-            <div class="middle">
-            <h2>Welcome!</h2>
-            </div>
+                    <div class="middle">
+                        <h2>Welcome!</h2>
+                    </div>
         </header>
     )
 }
